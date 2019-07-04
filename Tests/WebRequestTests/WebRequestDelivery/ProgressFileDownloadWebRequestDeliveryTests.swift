71337b046ca69bfdbdf186335a0ceba63c682911
John Heryer <john.heryer@vmlyr.com>
2019-07-03 21:55:02 -0500

    Test Refactor

--- Tests/WebRequestTests/WebRequestDelivery/ProgressFileDownloadWebRequestDeliveryTests.swift
+++ Tests/WebRequestTests/WebRequestDelivery/ProgressFileDownloadWebRequestDeliveryTests.swift
@@ -1,170 +1,120 @@
-//
-//  ProgressFileDownloadWebRequestDeliveryTests.swift
-//  WebRequestTests
-//
-//  Created by John Heryer on 7/3/19.
-//  Copyright © 2019 VML, Inc. All rights reserved.
-//
-
 import XCTest
 import WebRequest
 
 class ProgressFileDownloadWebRequestDeliveryTests: XCTestCase {
 
-   
+    var testPercent: Int = -99
+    var testfilePath: URL?
+    var testStatus: Int = -99
+    var expectation:XCTestExpectation?
+    var webRequest: WebRequest?
+    let subject =  ProgressFileDownloadWebRequestDelivery(targetURL: URL(string: "test.file")!)
+    var urlSession: MockURLSession?
+    var successfulResponse: HTTPURLResponse?
+    var unsuccessfulResponse: HTTPURLResponse?
+    var downloadRequest: URLSessionDownloadTask?
+    let testURL = URL(string: "test.file")!
+
+    override func setUp() {
+        testPercent = -9999
+        testfilePath = nil
+        testStatus = -999
+        urlSession = MockURLSession(configuration: URLSessionConfiguration())
+        successfulResponse = HTTPURLResponse(url: testURL, statusCode: 200, httpVersion: nil, headerFields: nil)
+        unsuccessfulResponse = HTTPURLResponse(url: testURL, statusCode: 404, httpVersion: nil, headerFields: nil)
+        downloadRequest = MockURLSessionDownloadTask(mockSession: urlSession!, url: testURL)
+        urlSession!.mockResponse = successfulResponse
+        
+        webRequest = WebRequest(urlString: "https://path/to/pdf.pdf",
+                            method: .GET,
+                            delivery: subject,
+                            onDataReceived: onDataReceived)
+        
+         subject.webRequest = webRequest;
+    }
+    
+    func onDataReceived(result: WebRequest.Result, request: WebRequest, percentComplete: Int, filePath: URL) {
+        testPercent = percentComplete
+        testfilePath = filePath
+        testStatus = result.status
+        self.expectation!.fulfill()
+    }
 
     func test_given_ProgressFileDownloadWebRequestDelivery_WHEN_zero_bytes_are_written_THEN_percentComplete_is_0() {
         
-        let expectation = self.expectation(description: "test_given_ProgressFileDownloadWebRequestDelivery_WHEN_zero_bytes_are_written_THEN_percentComplete_is_0")
-        let successfulResponse = HTTPURLResponse(url: URL(string: "test.file")!, statusCode: 200, httpVersion: nil, headerFields: nil)
-        let urlSession = MockURLSession(configuration: URLSessionConfiguration())
-        urlSession.mockResponse = successfulResponse
-        let downloadRequest = MockURLSessionDownloadTask(mockSession: urlSession, url: URL(string: "test.file")!)
-        let subject =  ProgressFileDownloadWebRequestDelivery(targetURL: URL(string: "test.file")!)
-        var testPercent: Int = -99
-        var testfilePath: URL?
-        let wr = WebRequest(urlString: "https://path/to/pdf.pdf",
-                                         method: .GET,
-                                         delivery: subject,
-                                         onDataReceived: {
-                                            (result, request, percentComplete, filePath) in
-                                            
-                                            print(filePath)
-                                            testPercent = percentComplete
-                                            testfilePath = filePath
-                                            expectation.fulfill()
-        })
-        
-        subject.webRequest = wr;
-        subject.urlSession(urlSession, downloadTask: downloadRequest, didWriteData: 0, totalBytesWritten: 0, totalBytesExpectedToWrite: 100)
+        expectation = self.expectation(description: "test_given_ProgressFileDownloadWebRequestDelivery_WHEN_zero_bytes_are_written_THEN_percentComplete_is_0")
+       
+        subject.urlSession(urlSession!, downloadTask: downloadRequest!, didWriteData: 0, totalBytesWritten: 0, totalBytesExpectedToWrite: 100)
         
         waitForExpectations(timeout: 10.0) { _ in
-            XCTAssertEqual(testPercent, 0)
-            XCTAssertEqual(testfilePath!.absoluteString, "test.file")
+            XCTAssertEqual(self.testPercent, 0)
+            XCTAssertEqual(self.testStatus, 200)
+            XCTAssertEqual(self.testfilePath!.absoluteString, "test.file")
         }
     }
     
     func test_given_ProgressFileDownloadWebRequestDelivery_WHEN_20_bytes_are_written_out_of_100_THEN_percentComplete_is_20() {
         
-        let expectation = self.expectation(description: "test_given_ProgressFileDownloadWebRequestDelivery_WHEN_20_bytes_are_written_out_of_100_THEN_percentComplete_is_20")
-        let successfulResponse = HTTPURLResponse(url: URL(string: "test.file")!, statusCode: 200, httpVersion: nil, headerFields: nil)
-        let urlSession = MockURLSession(configuration: URLSessionConfiguration())
-        urlSession.mockResponse = successfulResponse
-        let downloadRequest = MockURLSessionDownloadTask(mockSession: urlSession, url: URL(string: "test.file")!)
-        let subject =  ProgressFileDownloadWebRequestDelivery(targetURL: URL(string: "test.file")!)
-        var testPercent: Int = -99
-        var testfilePath: URL?
-        let wr = WebRequest(urlString: "https://path/to/pdf.pdf",
-                            method: .GET,
-                            delivery: subject,
-                            onDataReceived: {
-                                (result, request, percentComplete, filePath) in
-                                
-                                print(filePath)
-                                testPercent = percentComplete
-                                testfilePath = filePath
-                                expectation.fulfill()
-        })
-        
-        subject.webRequest = wr;
-        subject.urlSession(urlSession, downloadTask: downloadRequest, didWriteData: 0, totalBytesWritten: 20, totalBytesExpectedToWrite: 100)
+        expectation = self.expectation(description: "test_given_ProgressFileDownloadWebRequestDelivery_WHEN_20_bytes_are_written_out_of_100_THEN_percentComplete_is_20")
+
+        subject.urlSession(urlSession!, downloadTask: downloadRequest!, didWriteData: 0, totalBytesWritten: 20, totalBytesExpectedToWrite: 100)
         
         waitForExpectations(timeout: 10.0) { _ in
-            XCTAssertEqual(testPercent, 20)
-            XCTAssertEqual(testfilePath!.absoluteString, "test.file")
+            XCTAssertEqual(self.testPercent, 20)
+            XCTAssertEqual(self.testStatus, 200)
+            XCTAssertEqual(self.testfilePath!.absoluteString, "test.file")
         }
     }
     
     func test_given_ProgressFileDownloadWebRequestDelivery_WHEN_91_bytes_are_written_out_of_100_THEN_percentComplete_is_20() {
         
-        let expectation = self.expectation(description: "test_given_ProgressFileDownloadWebRequestDelivery_WHEN_20_bytes_are_written_out_of_100_THEN_percentComplete_is_20")
-        let successfulResponse = HTTPURLResponse(url: URL(string: "test.file")!, statusCode: 200, httpVersion: nil, headerFields: nil)
-        let urlSession = MockURLSession(configuration: URLSessionConfiguration())
-        urlSession.mockResponse = successfulResponse
-        let downloadRequest = MockURLSessionDownloadTask(mockSession: urlSession, url: URL(string: "test.file")!)
-        let subject =  ProgressFileDownloadWebRequestDelivery(targetURL: URL(string: "test.file")!)
-        var testPercent: Int = -99
-        var testfilePath: URL?
-        let wr = WebRequest(urlString: "https://path/to/pdf.pdf",
-                            method: .GET,
-                            delivery: subject,
-                            onDataReceived: {
-                                (result, request, percentComplete, filePath) in
-                                
-                                print(filePath)
-                                testPercent = percentComplete
-                                testfilePath = filePath
-                                expectation.fulfill()
-        })
-        
-        subject.webRequest = wr;
-        subject.urlSession(urlSession, downloadTask: downloadRequest, didWriteData: 0, totalBytesWritten: 91, totalBytesExpectedToWrite: 100)
+        expectation = self.expectation(description: "test_given_ProgressFileDownloadWebRequestDelivery_WHEN_20_bytes_are_written_out_of_100_THEN_percentComplete_is_20")
+        subject.urlSession(urlSession!, downloadTask: downloadRequest!, didWriteData: 0, totalBytesWritten: 91, totalBytesExpectedToWrite: 100)
         
         waitForExpectations(timeout: 10.0) { _ in
-            XCTAssertEqual(testPercent, 91)
-            XCTAssertEqual(testfilePath!.absoluteString, "test.file")
+            XCTAssertEqual(self.testPercent, 91)
+            XCTAssertEqual(self.testStatus, 200)
+            XCTAssertEqual(self.testfilePath!.absoluteString, "test.file")
         }
     }
 
     func test_given_ProgressFileDownloadWebRequestDelivery_WHEN_didFinishDownloading_THEN_percentComplete_is_20() {
         
-        let expectation = self.expectation(description: "test_given_ProgressFileDownloadWebRequestDelivery_WHEN_20_bytes_are_written_out_of_100_THEN_percentComplete_is_20")
-        let successfulResponse = HTTPURLResponse(url: URL(string: "test.file")!, statusCode: 200, httpVersion: nil, headerFields: nil)
-        let urlSession = MockURLSession(configuration: URLSessionConfiguration())
-        urlSession.mockResponse = successfulResponse
-        let downloadRequest = MockURLSessionDownloadTask(mockSession: urlSession, url: URL(string: "test.file")!)
-        let subject =  ProgressFileDownloadWebRequestDelivery(targetURL: URL(string: "test.file")!)
-        var testPercent: Int = -99
-        var testfilePath: URL?
-        let wr = WebRequest(urlString: "https://path/to/pdf.pdf",
-                            method: .GET,
-                            delivery: subject,
-                            onDataReceived: {
-                                (result, request, percentComplete, filePath) in
-                                
-                                print(filePath)
-                                testPercent = percentComplete
-                                testfilePath = filePath
-                                expectation.fulfill()
-        })
+        expectation = self.expectation(description: "test_given_ProgressFileDownloadWebRequestDelivery_WHEN_20_bytes_are_written_out_of_100_THEN_percentComplete_is_20")
         
-        subject.webRequest = wr;
-        subject.urlSession(urlSession, downloadTask: downloadRequest, didFinishDownloadingTo: URL(string: "test.file")!)
+        subject.urlSession(urlSession!, downloadTask: downloadRequest!, didFinishDownloadingTo: testURL)
         
         waitForExpectations(timeout: 10.0) { _ in
-            XCTAssertEqual(testPercent, 100)
-            XCTAssertEqual(testfilePath!.absoluteString, "test.file")
+            XCTAssertEqual(self.testPercent, 100)
+            XCTAssertEqual(self.testStatus, 200)
+            XCTAssertEqual(self.testfilePath!.absoluteString, "test.file")
+        }
+    }
+    
+    func test_given_ProgressFileDownloadWebRequestDelivery_WHEN_didFinishDownloading_AND_response_is_200_percentComplete_is_100() {
+        
+        expectation = self.expectation(description: "test_given_ProgressFileDownloadWebRequestDelivery_WHEN_20_bytes_are_written_out_of_100_THEN_percentComplete_is_20")
+        
+        subject.urlSession(urlSession!, downloadTask: downloadRequest!, didFinishDownloadingTo: testURL)
+        
+        waitForExpectations(timeout: 10.0) { _ in
+            XCTAssertEqual(self.testPercent, 100)
+            XCTAssertEqual(self.testStatus, 200)
+            XCTAssertEqual(self.testfilePath!.absoluteString, "test.file")
         }
     }
     
     func test_given_ProgressFileDownloadWebRequestDelivery_WHEN_didFinishDownloading_AND_response_is_not_200_percentComplete_is_0() {
         
-        let expectation = self.expectation(description: "test_given_ProgressFileDownloadWebRequestDelivery_WHEN_20_bytes_are_written_out_of_100_THEN_percentComplete_is_20")
-        let successfulResponse = HTTPURLResponse(url: URL(string: "test.file")!, statusCode: 404, httpVersion: nil, headerFields: nil)
-        let urlSession = MockURLSession(configuration: URLSessionConfiguration())
-        urlSession.mockResponse = successfulResponse
-        let downloadRequest = MockURLSessionDownloadTask(mockSession: urlSession, url: URL(string: "test.file")!)
-        let subject =  ProgressFileDownloadWebRequestDelivery(targetURL: URL(string: "test.file")!)
-        var testPercent: Int = -99
-        var testfilePath: URL?
-        let wr = WebRequest(urlString: "https://path/to/pdf.pdf",
-                            method: .GET,
-                            delivery: subject,
-                            onDataReceived: {
-                                (result, request, percentComplete, filePath) in
-                                
-                                print(filePath)
-                                testPercent = percentComplete
-                                testfilePath = filePath
-                                expectation.fulfill()
-        })
+        expectation = self.expectation(description: "test_given_ProgressFileDownloadWebRequestDelivery_WHEN_20_bytes_are_written_out_of_100_THEN_percentComplete_is_20")
         
-        subject.webRequest = wr;
-        subject.urlSession(urlSession, downloadTask: downloadRequest, didFinishDownloadingTo: URL(string: "test.file")!)
+        urlSession!.mockResponse = unsuccessfulResponse
+        subject.urlSession(urlSession!, downloadTask: downloadRequest!, didFinishDownloadingTo: testURL)
         
         waitForExpectations(timeout: 10.0) { _ in
-            XCTAssertEqual(testPercent, 0)
-            XCTAssertEqual(testfilePath!.absoluteString, "test.file")
+            XCTAssertEqual(self.testPercent, 0)
+            XCTAssertEqual(self.testStatus, 404)
+            XCTAssertEqual(self.testfilePath!.absoluteString, "test.file")
         }
     }
     
